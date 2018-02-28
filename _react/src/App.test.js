db7c93051e31d713251ed004fb05e0115728a80b
Tim Zuercher <tim.zuercher@eagles.oc.edu>
2018-02-27 19:34:09 -0600

    oops

--- _react/src/App.test.js
+++ _react/src/App.test.js
@@ -6,4 +6,5 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  expect(1).toEqual(2);
 });
