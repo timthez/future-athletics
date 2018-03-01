f77811dbd6907e346b69801182d95798ff20c4bd
Phil Curtis <phil.curtis@eagles.oc.edu>
2018-03-01 00:38:21 -0600

    Add default route with temporary component

--- _react/src/App.js
+++ _react/src/App.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import logo from './panda.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
+// Temporary Component for route
+const Default = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <Router>
+          <Route path="/" component={Default}/>
+        </Router>
       </MuiThemeProvider>
     );
   }
