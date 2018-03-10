9384672820ffa129f4fd64058b2f0eb23659be2d
Ryan Parker <ryanparker400m@gmail.com>
2018-03-10 11:41:40 -0600

    Created initial login page

--- _react/src/components/pages/login_page/login_page.js
+++ _react/src/components/pages/login_page/login_page.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import UserStore from '../../../stores/user_store';
 import Layout from '../../shared/layout.js';
+import {Paper} from 'material-ui';
+import {TextField} from 'material-ui';
+import {RaisedButton} from 'material-ui';
+import {FlatButton} from 'material-ui';
+import './login_page.css';
 
 class LoginPage extends Component {
   store = UserStore.instance();
@@ -21,11 +26,17 @@ class LoginPage extends Component {
   render() {
     return (
       <Layout>
-        Login
-        <input type="string" name="user.username" data-type="string" onChange={this.store.updateFieldFromInput} value={this.state.user.username} ></input>
+        <Paper className="login-page" zDepth={3}>
+          <TextField className="login-user-text" hintText="Username or Email" floatingLabelText="Username or Email"/>
+          <TextField className="login-pass-text" hintText="Password" floatingLabelText="Password" type="password"/><br />
+          <RaisedButton className="login-login-btn" label="LOGIN" primary={true}/>
+          <FlatButton className="login-forgot-btn" label="Forgot Password" />
+        </Paper>
       </Layout>
     );
   }
+
+
 }
 
 export default LoginPage;
