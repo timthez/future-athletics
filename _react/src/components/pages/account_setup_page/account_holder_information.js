97479df84143bf6a466a5e3b62c7e315a1ac07ba
Jacob Falcon <jefalcon444@gmail.com>
2018-03-22 22:55:58 -0500

    added more specific fields for the account holder

--- _react/src/components/pages/account_setup_page/account_holder_information.js
+++ _react/src/components/pages/account_setup_page/account_holder_information.js
@@ -7,9 +7,19 @@ class AccountHolderInformation extends Component {
   render(){
     return(
         <Paper className="login-page" zDepth={3}>
-          <TextField className="login-user-text" hintText="Username" floatingLabelText="Username or Email"/>
+          <TextField className="account-user-text" hintText="Username" floatingLabelText="Username"/>
           <br/>
-          <TextField className="login-pass-text" hintText="Password" floatingLabelText="Password" type="password"/>
+          <TextField className="account-pass-text" hintText="Password" floatingLabelText="Password" type="password"/>
+          <br/>
+          <TextField className="account-email-text" hintText="Email" floatingLabelText="Email" type="email"/>
+          <br/>
+          <TextField className="account-first-name-text" hintText="First Name" floatingLabelText="First Name"/>
+          <br/>
+          <TextField className="account-last-name-text" hintText="last Name" floatingLabelText="Last Name"/>
+          <br/>
+          <TextField className="account-birthday-text" hintText="Birth Day" floatingLabelText="Birthday"/>
+          <br/>
+          <TextField className="account-phone-text" hintText="Phone Number" floatingLabelText="Phone Number"/>
         </Paper>   
       );     
     }
